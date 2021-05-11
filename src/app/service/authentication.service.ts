545e85c14d25d3db8688a16619964f03535f1f90
JiangqiLou <jiangqilou@gmail.com>
2021-05-11 13:34:01 +0800

    port

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,7 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
-const host = 'http://localhost:3001';
+const host = 'http://localhost:3000';
 @Injectable({
   providedIn: 'root'
 })
